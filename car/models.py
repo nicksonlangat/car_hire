205fe2be475e5067c6f0c45177706ba435c70e4f
LoyalFlames <nicksonlangat95@gmail.com>
2020-04-21 17:11:26 +0300

    initial

---
+++ car/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.urls import reverse 
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='car_images')
+    description = models.TextField()
+    daily_rent = models.IntegerField()
+
+    is_available = models.BooleanField()
+
+    def get_absolute_url(self):
+        return reverse('car-details', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    car = models.ForeignKey(Car,on_delete=models.CASCADE) 
+
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.EmailField()
+    customer_phone_number = models.TextField()
+
+    booking_start_date = models.DateField()
+    booking_end_date = models.DateField()
+    booking_message = models.TextField()
+
+    is_approved = models.BooleanField()
